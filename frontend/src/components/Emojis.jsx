5c77aab93a8ca6329e6b6375866fe5bf7bf228ed
FranklinSitienei <sitieneifranklin@gmail.com>
2025-05-24 14:43:56 +0300

    Ready to change lives, one line of code at a time!

--- frontend/src/components/Emojis.jsx
+++
@@ -1,9 +0,0 @@
-export const fetchEmojis = async () => {
-    try {
-      const response = await fetch('https://sifas-heart-foundation.onrender.com/api/emojis/emojis');
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error('Error fetching emojis:', error);
-    }
-  };
